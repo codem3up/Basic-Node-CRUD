819de3b68732c4305d64a75f5c96cfb7ee042472
codem3up <andrewjoelbecker@gmail.com>
2016-01-06 04:54:42 -0700

    Flightplan

--- flightplan.js
+++ flightplan.js
@@ -1,50 +1,57 @@
-// flightplan.js
 var plan = require('flightplan');
 
-// configuration
-plan.target('staging', {
-  host: '159.203.209.170',
-  username: 'deploy',
-  agent: process.env.SSH_AUTH_SOCK
-});
+var appName = 'node-app';
+var username = 'deploy';
+var startFile = 'bin/www';
 
-plan.target('production', [
+var tmpDir = appName+'-' + new Date().getTime();
+
+// configuration
+plan.target('staging', [
   {
-    host: '159.203.209.170',
-    username: 'deploy',
+    host: '104.131.93.214',
+    username: username,
     agent: process.env.SSH_AUTH_SOCK
   }
 ]);
 
-var tmpDir = 'example-com-' + new Date().getTime();
+plan.target('production', [
+  {
+    host: '104.131.93.215',
+    username: username,
+    agent: process.env.SSH_AUTH_SOCK
+  },
+//add in another server if you have more than one
+// {
+//   host: '104.131.93.216',
+//   username: username,
+//   agent: process.env.SSH_AUTH_SOCK
+// }
+]);
 
 // run commands on localhost
 plan.local(function(local) {
-  local.log('Run build');
-  local.exec('gulp build');
+  // uncomment these if you need to run a build on your machine first
+  // local.log('Run build');
+  // local.exec('gulp build');
 
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
-  // rsync files to all the target's remote hosts
+  // rsync files to all the destination's hosts
   local.transfer(filesToCopy, '/tmp/' + tmpDir);
 });
 
-// run commands on the target's remote hosts
+// run commands on remote hosts (destinations)
 plan.remote(function(remote) {
-  remote.log('Move folder to web root');
-  remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: 'www'});
+  remote.log('Move folder to root');
+  remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
   remote.rm('-rf /tmp/' + tmpDir);
 
   remote.log('Install dependencies');
-  remote.sudo('npm --production --prefix ~/' + tmpDir
-                            + ' install ~/' + tmpDir, {user: 'www'});
+  remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
 
   remote.log('Reload application');
-  remote.sudo('ln -snf ~/' + tmpDir + ' ~/example-com', {user: 'www'});
-  remote.sudo('pm2 reload example-com', {user: 'www'});
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
+  remote.exec('forever start ~/'+appName+'/'+startFile);
 });
-
-// run more commands on localhost afterwards
-plan.local(function(local) { /* ... */ });
-// ...or on remote hosts
-plan.remote(function(remote) { /* ... */ });
